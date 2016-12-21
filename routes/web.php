95ec2ffccae7bf68732cf4448875c9ca259b4667
Lalit Vijay <lalit.vijay952@gmail.com>
2016-12-21 20:23:00 +0530

    ignore nbproject

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::match(['get', 'post'], '/botman', 'BotManController@handle');
+//Route::match(['get', 'post'], '/botman', 'BotManController@handle');
